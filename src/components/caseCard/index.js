58b5f429a94dc97ac7f36113771d034bcf83286d
harshita91 <roonwal721972@gmail.com>
2025-02-04 14:51:12 +0530

    resolved mobile responsive bug (#673)
    Signed-off-by: harshita91 <roonwal721972@gmail.com>

--- src/components/caseCard/index.js
+++ src/components/caseCard/index.js
@@ -4,33 +4,30 @@ import Translate from "@docusaurus/Translate";
 import "./styles.scss";
 
 export default function CaseCard(props) {
-    const {
-        title = "",
-        subTitle = "",
-        date = "",
-        desc = "",
-        imgUrl = "",
-        link = "",
-    } = props;
-    const history = useHistory();
+  const { title, subTitle, date, desc, imgUrl, link } = props;
+  const history = useHistory();
 
-    return (
-        <div
-            className="case-card"
-            style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),url(${imgUrl})`,
-            }}
-        >
-            <h2 className="title" onClick={() => history.push(link)}>
-                {title}
-            </h2>
-            <hr />
-            <h3 className="sub">{subTitle}</h3>
-            <p className="desc">{desc}</p>
-            <div className="date">{date}</div>
-            <a className="button" type="button" onClick={() => history.push(link)}>
-                <Translate>READ CASE STUDY</Translate>
-            </a>
+  return (
+    <div
+      className="case-card"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${imgUrl})`,
+      }}
+    >
+      <div className="card-content">
+        <h2 className="title" onClick={() => history.push(link)}>
+          {title}
+        </h2>
+        <hr />
+        {subTitle && <h3 className="sub">{subTitle}</h3>}
+        <p className="desc">{desc}</p>
+        <div className="bottom-section">
+          <span className="date">{date}</span>
+          <button className="button" onClick={() => history.push(link)}>
+            <Translate>READ CASE STUDY</Translate>
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
